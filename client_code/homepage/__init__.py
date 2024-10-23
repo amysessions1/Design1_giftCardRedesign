3d0ffef39c443e478e4451f91b16d408801c5234
nick_leggett@mines.edu <nick_leggett@mines.edu>
2024-10-23 22:11:25 +0000

    Created cardedit, homepage and homepage.RowTemplate1; Edited settings; Deleted Form1

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -1,14 +1,22 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import homepageTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..cardedit import cardedit
 
 
-class Form1(Form1Template):
+class homepage(homepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def view_cards_click(self, **event_args):
+    alert(
+      content=cardedit(),
+      title="Edit Cards",
+      large=True,
+    )
