7eb51bd0258dc02acdd0864e6d6ad4278bad8556
amysessions1@gmail.com <amysessions1@gmail.com>
2024-10-23 16:44:33 +0000

    Edited ServerModule1, Form1 and amy_example_implementation

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -14,3 +14,34 @@ class Form1(Form1Template):
     """This method is called when the button is clicked"""
     business_id = ["APPLE", "apple_company", "password"]
     anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company1", "password"]
+    anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company2", "password"]
+    anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company3", "password"]
+    anvil.server.call("add_business",business_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan1234", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12345", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12346", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12347", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+
+    print(anvil.server.call("get_business_data"))
+    print(anvil.server.call('get_user_data'))
+
+    print(anvil.server.call("user_login", "pianoMan12347", "password"))
+    print(anvil.server.call('business_login', "apple_company3", "password"))
+    
+
+
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -15,16 +15,27 @@ user_index = []
 balances = []
 unlinked_balances = []
 
+@anvil.server.callable
+def get_user_data():
+  return user_data
+
+@anvil.server.callable
+def get_business_data():
+  return business_data
+
 
 @anvil.server.callable
 def add_business(business_info):
   #business_info should be a list of info in the form [{business name}, {business_username}, {business_password}] all as strings
-  
   #generate business_id
   business_id = randrange(1000000000000000,9999999999999999)
   #check if business_id is already assigned to another business
   while business_id in business_index:
     business_id = randrange(1000000000000000,9999999999999999)
+  #check for unique username
+  for i in business_data:
+    if i[2] == business_info[1]:
+      return -1
   #add business to business_data
   add_data = [business_id, business_info[0], business_info[1], business_info[2]]
   business_data.append(add_data)
@@ -34,15 +45,21 @@ def add_business(business_info):
   balances.append([])
   #add business to unlinked_balances
   unlinked_balances.append([])
+  print("new business added")
+  return 0
 
+
+@anvil.server.callable
 def add_user(user_info):
   #user_info should be a list of info in the form [{user name}, {user_username}, {user_password}, {phone}] all as strings
-  
   #generate user_id
   user_id = randrange(1000000000000000,9999999999999999)
   #check if user_id is already assigned to another user
   while user_id in user_index:
     user_id = randrange(1000000000000000,9999999999999999)
+  for i in user_data:
+    if i[2] == user_info[1]:
+      return -1
   #add user to user_data
   add_data = [user_id, user_info[0], user_info[1], user_info[2], user_info[3]]
   user_data.append(add_data)
@@ -53,4 +70,32 @@ def add_user(user_info):
     balances[i].append(0)
   #add user to unlinked_balances
     unlinked_balances[i].append(0)
+  print("new user added")
+  return 0
 
+
+@anvil.server.callable
+def business_login(inp_username, inp_password):
+  j = business_data[1]
+  print(j)
+  for business in business_data:
+    if business[2] == inp_username:
+      if business[3] == inp_password:
+        return business[0]
+      else:
+        return 1
+    else:
+      return -1
+
+
+@anvil.server.callable
+def user_login(inp_username, inp_password):
+  print(user_data)
+  for user in user_data:
+    if user[2] == inp_username:
+      if user[3] == inp_password:
+        return user[0]
+      else:
+        return 1
+    else:
+      return -1
