f2ea452ddda00fd793b00bcc392e16a3a2e99a36
amysessions1@gmail.com <amysessions1@gmail.com>
2024-10-23 00:51:15 +0000

    Created amy_example_implementation; Edited ServerModule1 and Form1

--- client_code/amy_example_implementation/__init__.py
+++ client_code/amy_example_implementation/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import amy_example_implementationTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,14 +6,14 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class amy_example_implementation(amy_example_implementationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def amy_testing_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  def ex_add_business(self, **event_args):
+    """Example of adding a business"""
     business_id = ["APPLE", "apple_company", "password"]
     anvil.server.call("add_business",business_id)
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,24 +9,51 @@ from random import randrange
 #
 # To allow anvil.server.call() to call functions here, we mark
 # them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+
+business_data = []
+user_data = []
+business_index = []
+user_index = []
+balances = []
+unlinked_balances = []
+
+
 @anvil.server.callable
 def add_business(business_info):
   #business_info should be a list of info in the form [{business name}, {business_username}, {business_password}] all as strings
   
   #generate business_id
   business_id = randrange(1000000000000000,9999999999999999)
-  #add business to business_data table
-  add_data = []
-  add_data.append(business_id)
-  for i in range(3):
-    add_data.append(business_info[i])
-  app_tables.business_data.add_row(add_data)
+  #check if business_id is already assigned to another business
+  while business_id in business_index:
+    business_id = randrange(1000000000000000,9999999999999999)
+  #add business to business_data
+  add_data = [business_id, business_info[0], business_info[1], business_info[2]]
+  business_data.append(add_data)
+  #add business to business_index
+  business_index.append(business_id)
   #add business to balances table
+  balances.append([])
   #add business to unlinked_balances
+  unlinked_balances.append([])
+
+def add_user(user_info):
+  #user_info should be a list of info in the form [{user name}, {user_username}, {user_password}, {phone}] all as strings
+  
+  #generate user_id
+  user_id = randrange(1000000000000000,9999999999999999)
+  #check if user_id is already assigned to another user
+  while user_id in user_index:
+    user_id = randrange(1000000000000000,9999999999999999)
+  #add user to user_data
+  add_data = [user_id, user_info[0], user_info[1], user_info[2], user_info[3]]
+  user_data.append(add_data)
+  #add user to user_index
+  user_index.append(user_id)
+  #add user to balances table
+  for i in range(len(business_index)):
+    balances[i].append(0)
+  #add user to unlinked_balances
+    unlinked_balances[i].append(0)
+
