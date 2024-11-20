fb9398d99e42c389b94d662c6558fafff9084ba7
amysessions1@gmail.com <amysessions1@gmail.com>
2024-11-20 18:19:44 +0000

    Edited ServerModule1 and homepage

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -68,6 +68,60 @@ class homepage(homepageTemplate):
       large=True,
     )
 
+  def demo_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan1234", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12345", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12346", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    """Example of adding a user"""
+    user_id = ["Billy Joel", "pianoMan12347", "password", "3098675309"]
+    anvil.server.call("add_user",user_id)
+    
+    business_id = ["APPLE", "apple_company", "password"]
+    anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company1", "password"]
+    anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company2", "password"]
+    anvil.server.call("add_business",business_id)
+    """This method is called when the button is clicked"""
+    business_id = ["APPLE", "apple_company3", "password"]
+    anvil.server.call("add_business",business_id)
+
+
+    user = anvil.server.call("user_login", "pianoMan12345", "password")
+    comp = anvil.server.call('business_login', "apple_company2", "password")
+
+    anvil.server.call('add_funds', user, comp, 10.1)
+    anvil.server.call('add_funds', user, comp, 10.5)
+    user = anvil.server.call("user_login", "pianoMan12346", "password")
+    comp = anvil.server.call('business_login', "apple_company2", "password")
+    anvil.server.call('add_funds', user, comp, 10)
+    anvil.server.call('add_funds', user, comp, 15)
+    user = anvil.server.call("user_login", "pianoMan12347", "password")
+    comp = anvil.server.call('business_login', "apple_company", "password")
+    print(user, comp)
+    anvil.server.call('add_funds', user, comp, 10)
+    anvil.server.call('add_funds', user, comp, 15)
+
+    print(anvil.server.call('make_purchace', user, comp, 10))
+    print(anvil.server.call('make_purchace', user, comp, 20))
+    print(anvil.server.call('make_purchace', user, comp, 20))
+
+    print(anvil.server.call('get_user_info', user))
+    print(anvil.server.call('get_business_info', comp))
+
+    x = anvil.server.call('add_unlinked', comp, 20)
+
+    anvil.server.call('claim_unlinked', x, user)
+
 
 
 
